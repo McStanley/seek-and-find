cd0d9ae37ff7ba7645c2fe5ed2cd40127b80f1bf
Stanisław Olejniczak <0stanislawolejniczak0@gmail.com>
2023-03-14 17:13:48 +0100

    Add position data to wizards

--- src/utils/getWizardsData.js
+++ src/utils/getWizardsData.js
@@ -7,16 +7,34 @@ function getWizardsData() {
     {
       id: 1,
       img: Wizard1,
+      position: {
+        top: 0.58,
+        bottom: 0.636,
+        left: 0.572,
+        right: 0.604,
+      },
       active: true,
     },
     {
       id: 2,
       img: Wizard2,
+      position: {
+        top: 0.795,
+        bottom: 0.846,
+        left: 0.603,
+        right: 0.626,
+      },
       active: true,
     },
     {
       id: 3,
       img: Wizard3,
+      position: {
+        top: 0.259,
+        bottom: 0.308,
+        left: 0.854,
+        right: 0.88,
+      },
       active: true,
     },
   ];
